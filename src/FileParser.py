c4930632a4141fa028641c9745e9c4b626f6fd1f
Ethan <ethantaylor411@gmail.com>
2017-11-17 19:47:06 -0500

    added more comments

--- src/FileParser.py
+++ src/FileParser.py
@@ -10,22 +10,22 @@ def FileParser():
         self.index = 0
 
     def __str__(self): #toString() method for the parser just to show the fileSrc as String path
-        print(self.fileSrc)
+        print(self.fileSrc) # just a simple str method to print the file src 
 
     def hasNext(): #method to determine if the parser has reached the end of the file or not
         return self.index != self.fileLen #determine if it is the end of the file
     def parseFile(self):
-        self.data = self.fileSrc.read().split("'n")
-        self.fileLen = len(data)
+        self.data = self.fileSrc.read().split("'n") #split at new lines, may change based on pcap format
+        self.fileLen = len(data) # set the fileLen based on the lenght of the array we've gotten
         
-    def parseLine(self):
-        line = data[self.index].split(",") ##assume that it's CSV for now
-        self.index += 1
-        return line
+    def parseLine(self): #return one line of the file
+        line = data[self.index].split(",") ##assume that it's CSV for now, split into an array
+        self.index += 1 #increment the index so the hasNext() function can successfully test
+        return line #return the line as an array
 
-    def main():
+    def main(): ## Just a function to test the functionality of the FileParser, I think we can just print the lines here to test if they're actually being read and parsed
         fp = FileParser("test.pcap")
         fp.parseFile()
         while(fp.hasNext()):
-            print(fp.parseLine())
+            print(fp.parseLine()) # should return as an array of the line separated by the necessary delimeter above 
 
--- src/PacketGenerator.py
+++ src/PacketGenerator.py
@@ -3,21 +3,23 @@ import scapy.all
 
 class PacketGenerator:
     
-    def __init__(self, fileParser):
-        self.fileParser = fileParser
+    def __init__(self, fileParser): 
+        self.fileParser = fileParser # I was thinking we could just take a fileParser as an init paramter this way we would already have it
 
     def generate(self, arrData):
-
+        # generate a packet based on the arrData that we will receive from the file parser as an array of the line
+        #implementation to come
+        return 0 # return 0 for now while we're working on the implementation
     
     def getParser():
-        return self.fileParser
+        return self.fileParser # simple get method to return file parser for simplicity's sake when implementing our driver
 
     def __str__():
-        print("PacketGenerator")
+        print("PacketGenerator") # str function to prove there's a packet generator 
 
-    def main():
-        pg = PacketGenerator(FileParser("test.pcap"))
-        parser = pg.getParser()
-        while(parser.hasNext()):
-            arrData = parser.parseLine()
-            pg.generate(arrData)
+    def main(): # test function for our PacketGenerator
+        pg = PacketGenerator(FileParser("test.pcap")) # give it our parser with a test pcap file in our current directory
+        parser = pg.getParser() # simplify the name of the parser 
+        while(parser.hasNext()): # go through line by line
+            arrData = parser.parseLine() # get the line we need from the parser as an array
+            pg.generate(arrData) # generate a packet 
