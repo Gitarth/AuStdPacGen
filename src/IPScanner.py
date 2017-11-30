35e7e2ca2d492ed2028b270bf91b3db9cc4e8a8a
phitarth <patelhk@g.cofc.edu>
2017-11-29 20:46:11 -0500

    Fully Implemented IPScanner.py

--- src/IPScanner.py
+++ src/IPScanner.py
@@ -1,44 +1,32 @@
 """
 Python file to find online hosts
 """
-def main():
+def IPScanner():
     import subprocess
     import ipaddress
     from subprocess import Popen, PIPE
 
-    network = ipaddress.ip_network()
+    network = ipaddress.ip_network("10.0.0.0/24")
+
+    available_hosts = []
+    count = 0
 
     for i in network.hosts():
+
         i = str(i)
         toping = Popen(['ping','-c','3', '-W', '200', i], stdout = PIPE)
         output = toping.communicate()[0]
         hostalive = toping.returncode
         if hostalive == 0:
-            print (i, "is reachable")
-        else:
-            print(i, "is unreachable")
+            print (i, "is appended to the list.")
+            available_hosts.append(i)
+        #else:
 
+        #     print(i, "is unreachable")
+        count += 1
+        print("So far " + str(count) + " ips scanned.")
 
-
-
-# def scan_and_print_neighbors(net, interface, timeout=1):
-#     logger.info("arping %s on %s" % (net, interface))
-#     try:
-#         ans, unans = scapy.layers.l2.arping(net, iface=interface, timeout=timeout, verbose=True)
-#         for s, r in ans.res:
-#             line = r.sprintf("%Ether.src%  %ARP.psrc%")
-#             try:
-#                 hostname = socket.gethostbyaddr(r.psrc)
-#                 line += " " + hostname[0]
-#             except socket.herror:
-#                 # failed to resolve
-#                 pass
-#             logger.info(line)
-#     except socket.error as e:
-#         if e.errno == errno.EPERM:     # Operation not permitted
-#             logger.error("%s. Did you run as root?", e.strerror)
-#         else:
-#             raise
+    return available_hosts
 
 if __name__ == "__main__":
-    main()
+    IPScanner()
