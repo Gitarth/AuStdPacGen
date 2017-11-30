71372b65a6fdb92e4235a195ef6e47ad0ea7f6b1
phitarth <patelhk@g.cofc.edu>
2017-11-30 13:37:48 -0500

    Fully implemented pcap to json boiiii

--- src/pcaptojson.py
+++ src/pcaptojson.py
@@ -4,11 +4,11 @@ import os
 import subprocess
 
 def convert_pcap_to_json(file):
-    tshark_call = ["tshark", "-r", "-T", "json"]
-    tshark_in = open(file, "rb")
-    tshark_out = open(file+".json", "w")
+    tshark_in = file
+    tshark_out = open(file.split('.')[0]+".json", "w")
+    tshark_call = ["tshark", "-r",tshark_in, "-T", "json"]
 
-    tshark = run(tshark_call, stdin=tshark_in, stdout = tshark_out)
+    tshark = run(tshark_call, stdout = tshark_out)
 
 
 def main():
@@ -18,7 +18,7 @@ def main():
         filename = os.fsdecode(file)
         if filename.endswith(".pcapng") or filename.endswith(".pcap"):
             json_file = convert_pcap_to_json(file)
-            write(json_file)
+            ##write(json_file)
             continue
         else:
             continue
