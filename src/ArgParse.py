0fa03c769b2efba644438f9c573c4455bc929f53
phitarth <patelhk@g.cofc.edu>
2017-11-29 21:08:15 -0500

    edited argparse

--- src/ArgParse.py
+++ src/ArgParse.py
@@ -3,11 +3,12 @@ import argparse
 #%% Create parser
 def create_parser():
     parser = argparse.ArgumentParser( description = "Gold Standard Traffic Replayer")
-    parser.add_argument('-f', '--pcapfile', help="Enter pcap traffic file to replay")
+    parser.add_argument('-f', '--pcapfile', nargs="+", help="Enter pcap traffic file to replay")
     parser.add_argument('-ip', '--hostip', help="Enter your host ip")
-    parser.add_argument('-s', '--subnetmask', help="Enter your subnet mask")
+    parser.add_argument('-na','-netaddr', help="Enter network address with netmask")
     args = parser.parse_args()
     return args
+
 
 #%% Main
 def main():
