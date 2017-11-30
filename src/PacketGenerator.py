b55a0ae8c1e4b949d86461dc75319607372177d1
Ethan <hendrixet@g.cofc.edu>
2017-11-29 19:56:06 -0500

    is_local method appears to be working

--- src/PacketGenerator.py
+++ src/PacketGenerator.py
@@ -1,5 +1,5 @@
 from Remap import Remap
-from scapy.all import send,rdpcap, wrpcap
+from scapy.all import * #send,rdpcap, wrpcap
 """
 11/28/17
 Authors: Ethan Hendrix, Hitarth Patel
@@ -13,7 +13,7 @@ class PacketGenerator:
 
     def __init__(self):
         self.remapper = Remap() # I was thinking we could just take a fileParser as an init paramter this way we would already have it
-        self.local_ips = ["10.0.0.0","172.16.0.0","192.168.0.0"]
+        ## self.local_ips = ["10.0.0.0","172.16.0.0","192.168.0.0"]
         self.logPkts = []
 
 
@@ -23,9 +23,12 @@ class PacketGenerator:
 
         method to see if the packet is a local packet
         if it is a local packet we will remap it
+
+        This method will take a packet, split the destination into an array then check to see if they match a local IP pattern
         """
         try:
             dst = pkt[IP].dst.split(".")
+
             if(dst[0] == "10"):
                 return True
             elif(dst[0] == "172" and dst[1] == "16"):
@@ -35,15 +38,11 @@ class PacketGenerator:
             else:
                 return False
         except:
-            return False
-
-
-
-        return False
+                return False
 
     def generate(self, arrData):
         """
-        Generate packet functiont that will
+        Generate packet function that will
         """
         for pkt in arrData:
             if self.is_local(pkt):
@@ -73,11 +72,31 @@ class PacketGenerator:
         test function for our PacketGenerator
         arrData = [ source, destination, source_port, destination_port, protocol ]
         """
-        pg = PacketGenerator(Remap())
+
+        pg = PacketGenerator()
         fp = FileParser("testFiles/test00.pcapng")
         fp.parseFile()
         pkts = fp.getPktData()
-        pg.generate(pkts)
+        print('Testing is_local\n')
+        nonlocal_pkt = pkts[0]
+        local_pkt = pkts[1]
+        print("Testing non-local packet - Expecting boolean result to be False")
+        oracle = False
+        result = pg.is_local(nonlocal_pkt)
+        if(result == oracle):
+            print("Result = %s - Test Passed!\n",result)
+        else:
+            print("Result = %s - Test Failed!\n",result)
+        print("Testing local packet - Expecting boolean result to be True")
+        oracle = True
+        result = pg.is_local(local_pkt)
+        if(result == oracle):
+            print("Result = %s - Test Passed!\n")
+        else:
+            print("Result = %s - Test Failed!\n")
+
+        ##print("Testing ability to generate packets")
+        ##pg.generate(pkts)
 
 
 
